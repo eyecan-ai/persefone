15019b902db22a5daf9d466fdb31b21c5751557a
m4nh <degregorio.daniele@gmail.com>
2020-08-01 15:06:36 +0200

    EndpointDeepService

--- persefone/interfaces/grpc/servers/deep_services.py
+++ persefone/interfaces/grpc/servers/deep_services.py
@@ -1,6 +1,7 @@
+from persefone.interfaces.grpc.clients.deep_services import DeepServicePack
+from typing import Callable
+from schema import Schema
 from persefone.interfaces.proto.utils.comm import ResponseStatusUtils
-from persefone.data.io.drivers.common import AbstractFileDriver
-from persefone.data.databases.mongo.clients import MongoDatabaseClient
 from persefone.interfaces.grpc.deep_services_pb2_grpc import DeepServiceServicer, add_DeepServiceServicer_to_server
 from persefone.interfaces.grpc.deep_services_pb2 import (
     DDeepServiceRequest, DDeepServiceResponse
@@ -38,18 +39,65 @@ class EchoDeepService(ABC, DeepServiceServicer):
         return response
 
 
-class MongoDeepService(DeepService):
+class EndpointDeepService(ABC, DeepServiceServicer):
 
-    def __init__(self,
-                 mongo_client: MongoDatabaseClient,
-                 driver: AbstractFileDriver,
-                 ):
-        super(MongoDeepService, self).__init__()
+    def __init__(self):
+        self._callbacks_map = {}
+        self._schema_map = {}
+        self._output_schema_map = {}
 
-        self._mongo_client = mongo_client
-        assert isinstance(driver, AbstractFileDriver), f"Invalid driver: {driver}"
-        self._drivers = [driver]
+    def register_endpoint(self,
+                          name: str,
+                          schema: Schema,
+                          callback: Callable[[DeepServicePack], DeepServicePack],
+                          output_schema: Schema = None
+                          ):
 
-    def DeepServe(self, request, context):
-        print("REUQEST", request)
-        return DDeepServiceResponse()
+        self._callbacks_map[name] = callback
+        self._schema_map[name] = schema
+        if output_schema is not None:
+            self._output_schema_map[name] = output_schema
+
+    def register(self, grpc_server):
+        add_DeepServiceServicer_to_server(self, grpc_server)
+
+    def _error_response(self, message: str):
+        response = DDeepServiceResponse()
+        response.status.CopyFrom(ResponseStatusUtils.create_error_status(message))
+        return response
+
+    def DeepServe(self, request: DDeepServiceRequest, context: grpc.ServicerContext) -> DDeepServiceResponse:
+
+        # Pack from request
+        try:
+            pack = DeepServicePack.from_deep_service_request(request)
+        except Exception as e:
+            return self._error_response(str(e))
+
+        # Iterate available schema
+        for name, schema in self._schema_map.items():
+            # If schema is valid, invoke corresponding callback if any
+            if schema.is_valid(pack.metadata):
+
+                # Callback found
+                if name in self._callbacks_map:
+
+                    reply_pack = self._callbacks_map[name](pack)
+
+                    # Validate output metadata before send
+                    if name in self._output_schema_map:
+                        # try:
+                        #     print(self._output_schema_map[name].validate(reply_pack.metadata))
+                        # except Exception as e:
+                        #     return self._error_response(str(e))
+                        if not self._output_schema_map[name].is_valid(reply_pack.metadata):
+                            return self._error_response(f'Server produces an invalid response for [{name}]')
+
+                    return reply_pack.to_response()
+
+                # Callback not found
+                else:
+                    return self._error_response(f"Service logic ['{name}'] is not implemented yet!")
+
+        # No valid schema found!
+        return self._error_response(f"Invalid input metadata for [{pack.metadata}]")
