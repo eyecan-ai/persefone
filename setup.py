8c58803bbffd9255399d1229982d927ca2665bb4
m4nh <degregorio.daniele@gmail.com>
2020-12-04 15:11:54 +0100

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/m4nh/persefone',
-    version='0.0.11',
+    version='0.0.12',
     zip_safe=False,
 )
